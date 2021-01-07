aba3b11503fd5c5629614d28bc19a825c5dd6d66
Luca Cavretti <cavretiluca@live.it>
2021-01-07 01:32:59 +0100

    Finished Exercise

---
+++ index.php
@@ -0,0 +1,155 @@
+<?php
+
+$db = [
+    [
+        'question' => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
+        'answer' => "La <a href='#'>recente decisione della Corte di giustizia dell'Unione europea</a> ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi. <br><br>
+        
+        Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.<br><br>
+        
+        Per presentare una richiesta di rimozione, compila questo <a href='#'>modulo web</a>. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.<br><br>
+        
+        Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.<br><br>
+        
+        Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione."
+    ],
+    [
+        'question' => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
+        'answer' => "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.
+        <br><br>
+        Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.<br><br>
+        Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel <a href='#'>Centro Google per la sicurezza online.</a>.<br><br>
+        <a href='#'>Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo."
+    ],
+    [
+        'question' => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
+        'answer' => "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi <a href='#'>contattare il  webmaster</a> del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai <a href='#'>click qui</a>. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile <a href='#'>visitare la nostra pagina di assistenza per avere ulteriori informazioni</a>"
+    ]
+];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google Faq</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+<link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+<style>
+    <?php include 'style.css'; ?>
+</style>
+    <header>
+        <div class="header-top">
+            <div class='miologo'>
+                <a href="#"><img src="img/logo.png" alt="Google"></a>
+                <h3><a href="https://policies.google.com/">Privacy e termini</a></h3>
+            </div>
+            <div>
+                <a href="#"><i class="fas fa-th"></i></a>
+                <a href="#"><img class='propic' src="img/me.png" alt="you"></a>
+            </div>
+        </div>
+    <div class="navbar">
+        <ul>
+            <li><a href="#">Introduzione</a></li>
+            <li><a href="#">Norme sulla privacy</a></li>
+            <li><a href="#">Termini di servizio</a></li>
+            <li><a href="#">Tecnologie</a></li>
+            <li><a href="#">Domande frequenti</a></li>
+        </ul>
+    </div>
+    </header>
+    <main>
+        <div class="wrapper">
+            <?php foreach ($db as $el) { ?> 
+            <h2 class='title'><?= $el["question"] ?></h2>
+            <p><?= $el["answer"] ?></p>
+            <?php } ?>
+        </div>
+    </main>
+    <div class="wrapper">
+    <footer>
+        <div class="link">
+            <ul>
+                <li><a href="#">Google •</a></li>
+                <li><a href="#">Tutto su Google •</a></li>
+                <li><a href="#">Privacy •</a></li>
+                <li><a href="#">Termini •</a></li>
+            </ul>
+        </div>
+        <div class="lang">
+            <img src="img/lang.png" alt="world">
+            <select name="languages" id="lang">
+                <option>Afrikaans</option>
+                <option>Bahasa Indonesia</option>
+                <option>Bahasa Melayu</option>
+                <option>Català</option>
+                <option>Čeština</option>
+                <option>Dansk</option>
+                <option>Deutsch</option>
+                <option>Eesti</option>
+                <option>English</option>
+                <option>English (United Kingdom)</option>
+                <option>Español</option>
+                <option>Español (Latinoamérica)</option>
+                <option>Euskara</option>
+                <option>Filipino</option>
+                <option>Français</option>
+                <option>Français (Canada)</option>
+                <option>Galego</option>
+                <option>Hrvatski</option>
+                <option>Isizulu</option>
+                <option>Íslenska</option>
+                <option selected="" value="it">Italiano</option>
+                <option>Kiswahili</option>
+                <option>Latviešu</option>
+                <option>Lietuvių</option>
+                <option>Magyar</option>
+                <option>Nederlands</option>
+                <option>Norsk</option>
+                <option>Polski</option>
+                <option>Português (Brasil)</option>
+                <option>Português (Portugal)</option>
+                <option>Română</option>
+                <option>Slovenčina</option>
+                <option>Slovenščina</option>
+                <option>Srpski</option>
+                <option>Suomi</option>
+                <option>Svenska</option>
+                <option>Tiếng Việt</option>
+                <option>Türkçe</option>
+                <option>Ελληνικά</option>
+                <option>Български</option>
+                <option>Русский</option>
+                <option>Српски</option>
+                <option>Українська</option>
+                <option>‫עברית‬</option>
+                <option>‫اردو‬</option>
+                <option>‫العربية‬</option>
+                <option>‫فارسی‬</option>
+                <option>አማርኛ</option>
+                <option>मराठी</option>
+                <option>हिन्दी</option>
+                <option>বাংলা</option>
+                <option>ગુજરાતી</option>
+                <option>தமிழ்</option>
+                <option>తెలుగు</option>
+                <option>ಕನ್ನಡ</option>
+                <option>മലയാളം</option>
+                <option>ไทย</option>
+                <option>한국어</option>
+                <option>中文 (香港)</option>
+                <option>中文（简体中文）</option>
+                <option>中文（繁體中文）</option>
+                <option>日本語</option>
+            </select>
+        </div>
+    </footer>
+    </div>
+    
+
+
+</body>
+</html>
